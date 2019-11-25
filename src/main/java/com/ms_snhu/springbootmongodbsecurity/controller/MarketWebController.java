ac49ec3f2158c507cc1649e15bb23895eb5f601b
Matthew Spencer <matthew.spencer@accenture.com>
2019-11-25 15:48:53 -0600

    Security and Modularity enhancements
    Added better security control for user vs admin
    Created Fragments to better share functionality between pages

--- src/main/java/com/ms_snhu/springbootmongodbsecurity/controller/MarketWebController.java
+++ src/main/java/com/ms_snhu/springbootmongodbsecurity/controller/MarketWebController.java
@@ -33,7 +33,6 @@ import com.ms_snhu.springbootmongodbsecurity.domain.SectorIndustryOutstandingSha
 import com.ms_snhu.springbootmongodbsecurity.domain.Stocks;
 import com.ms_snhu.springbootmongodbsecurity.domain.User;
 import com.ms_snhu.springbootmongodbsecurity.repository.MarketRepository;
-import com.ms_snhu.springbootmongodbsecurity.repository.RoleRepository;
 
 /**
  * Controller Class for HTTP web services
@@ -46,191 +45,209 @@ import com.ms_snhu.springbootmongodbsecurity.repository.RoleRepository;
  */
 @Controller
 public class MarketWebController {
-	@Autowired
-	private MongoTemplate marketTemplate;
-	@Autowired
-	private ControllerHelper marketHelper;
-	@Autowired
-	private MarketRepository marketRepository;
-	@Autowired
-	private RoleRepository roleRpository;
+  @Autowired
+  private MongoTemplate    marketTemplate;
+  @Autowired
+  private ControllerHelper marketHelper;
+  @Autowired
+  private MarketRepository marketRepository;
 
-	@Value("${snhu.dateFormat}")
-	private String dateFormat;
+  @Value("${snhu.dateFormat}")
+  private String dateFormat;
+  private String adminDash = "dashboard";
+  private String userDash = "userView";
 
-	@GetMapping(value = {"/dashboard","/userView"})
-    public ModelAndView dashboard() {
-        ModelAndView modelAndView = new ModelAndView();
-        
-        String stocksCollectionName = marketTemplate.getCollectionName(Stocks.class);
-        
-        List<String> industries = marketHelper
-				.convertIterableToList(marketTemplate.getCollection(stocksCollectionName).distinct("Industry", String.class));
-		List<String> sectors = marketHelper
-				.convertIterableToList(marketTemplate.getCollection(stocksCollectionName).distinct("Sector", String.class));
-        
-        User user = marketHelper.getAuthenticatedUser();
-        modelAndView.addObject("currentUser", user);
-        modelAndView.addObject("fullName", user.getFullname());
-        modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
-        modelAndView.addObject("industries",industries);
-        modelAndView.addObject("sectors",sectors);
-        modelAndView.addObject("isFiftyDay",false);
-        modelAndView.addObject("isIndustry",false);
-        if(user.getRoles().contains(roleRpository.findByRole("ADMIN"))) {
-        	modelAndView.setViewName("dashboard");
-        } else {
-        	modelAndView.setViewName("userView");
-        }
-        return modelAndView;
+  @GetMapping(value = {"/dashboard", "/userView"})
+  public ModelAndView dashboard() {
+    ModelAndView modelAndView = new ModelAndView();
+
+    String stocksCollectionName = marketTemplate.getCollectionName(Stocks.class);
+
+    List<String> industries = marketHelper.convertIterableToList(marketTemplate.getCollection(stocksCollectionName).distinct("Industry", String.class));
+    List<String> sectors = marketHelper.convertIterableToList(marketTemplate.getCollection(stocksCollectionName).distinct("Sector", String.class));
+
+    User user = marketHelper.getAuthenticatedUser();
+    modelAndView.addObject("currentUser", user);
+    modelAndView.addObject("fullName", user.getFullname());
+    modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
+    modelAndView.addObject("industries", industries);
+    modelAndView.addObject("sectors", sectors);
+    modelAndView.addObject("isFiftyDay", false);
+    modelAndView.addObject("isIndustry", false);
+    if (marketHelper.isUserAdmin(user)) {
+      modelAndView.addObject("dashView", adminDash);
+      modelAndView.setViewName( adminDash);
     }
-	
-	
-	@GetMapping("/industryPerformance")
-	public String industryPerformance(@RequestParam(name = "industry", required = true) String industry,
-			@RequestParam(name = "performers", required = true) Integer performers, Model model) {
+    else {
+      modelAndView.addObject("dashView", userDash);
+      modelAndView.setViewName(userDash);
+    }
+    return modelAndView;
+  }
 
-		String stocksCollectionName = marketTemplate.getCollectionName(Stocks.class);
-		
-		List<String> industries = marketHelper
-				.convertIterableToList(marketTemplate.getCollection(stocksCollectionName).distinct("Industry", String.class));
+  @GetMapping("/industryPerformance")
+  public ModelAndView industryPerformance(@RequestParam(name = "industry", required = true) String industry, @RequestParam(name = "performers", required = true) Integer performers, Model model) {
+    ModelAndView modelAndView = new ModelAndView();
+    String stocksCollectionName = marketTemplate.getCollectionName(Stocks.class);
 
-		Sort performanceSort = new Sort(Sort.Direction.DESC, "Performance (YTD)");
-		Query industryQuery = new Query(Criteria.where("Industry").is(industry)).with(performanceSort)
-				.limit(performers);
-		List<Stocks> stocks = marketTemplate.find(industryQuery, Stocks.class, stocksCollectionName);
+    List<String> industries = marketHelper.convertIterableToList(marketTemplate.getCollection(stocksCollectionName).distinct("Industry", String.class));
 
-		User user = marketHelper.getAuthenticatedUser();
+    Sort performanceSort = new Sort(Sort.Direction.DESC, "Performance (YTD)");
+    Query industryQuery = new Query(Criteria.where("Industry").is(industry)).with(performanceSort).limit(performers);
+    List<Stocks> stocks = marketTemplate.find(industryQuery, Stocks.class, stocksCollectionName);
 
-		model.addAttribute("industries", industries);
-		model.addAttribute("industry", industry);
-		model.addAttribute("performers", performers);
-		model.addAttribute("currentUser", user);
-		model.addAttribute("fullName", user.getFullname());
-		model.addAttribute("stocks", stocks);
+    User user = marketHelper.getAuthenticatedUser();
 
-		model.addAttribute("isIndustry", true);
-		model.addAttribute("isFiftyDay", false);
-		return "dashboard.html";
-	}
+    modelAndView.addObject("industries", industries);
+    modelAndView.addObject("industry", industry);
+    modelAndView.addObject("performers", performers);
+    modelAndView.addObject("currentUser", user);
+    modelAndView.addObject("fullName", user.getFullname());
+    modelAndView.addObject("stocks", stocks);
 
-	@GetMapping("/fiftyDayMovingAverage")
-	public String movingAverage(@RequestParam(name = "lowerBound", required = true) Double lowerBound,
-			@RequestParam(name = "upperBound", required = true) Double upperBound, Model model) {
+    modelAndView.addObject("isIndustry", true);
+    modelAndView.addObject("isFiftyDay", false);
+    
+    if (marketHelper.isUserAdmin(user)) {
+      modelAndView.addObject("dashView", adminDash);
+      modelAndView.setViewName( adminDash);
+    }
+    else {
+      modelAndView.addObject("dashView", userDash);
+      modelAndView.setViewName(userDash);
+    }
+    return modelAndView;
+  }
 
-		String stocksCollectionName = marketTemplate.getCollectionName(Stocks.class);
-		
-		Sort smaSort = new Sort(Sort.Direction.ASC, "50-Day Simple Moving Average");
-		Query fiftyDayQuery = new Query(Criteria.where("50-Day Simple Moving Average").gt(lowerBound).lt(upperBound))
-				.with(smaSort);
-		List<Stocks> stocks = marketTemplate.find(fiftyDayQuery, Stocks.class, stocksCollectionName);
+  @GetMapping("/fiftyDayMovingAverage")
+  public ModelAndView movingAverage(@RequestParam(name = "lowerBound", required = true) Double lowerBound, @RequestParam(name = "upperBound", required = true) Double upperBound, Model model) {
+    ModelAndView modelAndView = new ModelAndView();
+    String stocksCollectionName = marketTemplate.getCollectionName(Stocks.class);
 
-		User user = marketHelper.getAuthenticatedUser();
+    Sort smaSort = new Sort(Sort.Direction.ASC, "50-Day Simple Moving Average");
+    Query fiftyDayQuery = new Query(Criteria.where("50-Day Simple Moving Average").gt(lowerBound).lt(upperBound)).with(smaSort);
+    List<Stocks> stocks = marketTemplate.find(fiftyDayQuery, Stocks.class, stocksCollectionName);
 
-		model.addAttribute("stocks", stocks);
-		model.addAttribute("lowerBound", lowerBound);
-		model.addAttribute("upperBound", upperBound);
-		model.addAttribute("currentUser", user);
-		model.addAttribute("fullName", user.getFullname());
+    User user = marketHelper.getAuthenticatedUser();
 
-		model.addAttribute("isFiftyDay", true);
-		model.addAttribute("isIndustry", false);
-		return "dashboard.html";
-	}
+    modelAndView.addObject("stocks", stocks);
+    modelAndView.addObject("lowerBound", lowerBound);
+    modelAndView.addObject("upperBound", upperBound);
+    modelAndView.addObject("currentUser", user);
+    modelAndView.addObject("fullName", user.getFullname());
 
-	@GetMapping("/insertNewStock")
-	public ModelAndView insertNewStock(Model model) {
+    modelAndView.addObject("isFiftyDay", true);
+    modelAndView.addObject("isIndustry", false);
+    if (marketHelper.isUserAdmin(user)) {
+      modelAndView.addObject("dashView", adminDash);
+      modelAndView.setViewName( adminDash);
+    }
+    else {
+      modelAndView.addObject("dashView", userDash);
+      modelAndView.setViewName(userDash);
+    }
+    return modelAndView;
+  }
 
-		User user = marketHelper.getAuthenticatedUser();
-		Stocks stock = new Stocks();
-		stock.set_id(ObjectId.get().toString());
-		
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("currentUser",user);
-		mv.addObject("stock", stock);
-		mv.addObject("dateFormat", dateFormat);
-		mv.addObject("insertUpdate", "Insert");
-		mv.addObject("fullName", user.getFullname());
-		
-		mv.setViewName("insertUpdate");
-		
-		return mv;
-	}
+  @GetMapping("/insertNewStock")
+  public ModelAndView insertNewStock(Model model) {
 
-	@GetMapping("/updateStock")
-	public String updateStock(@RequestParam(name = "ticker", required = true) String ticker, Model model) {
+    User user = marketHelper.getAuthenticatedUser();
+    Stocks stock = new Stocks();
+    stock.set_id(ObjectId.get().toString());
 
-		User user = marketHelper.getAuthenticatedUser();
-		Query query = new Query(Criteria.where("ticker").is(ticker));
-		Stocks stock = marketTemplate.findOne(query, Stocks.class);
-		model.addAttribute("stock", stock);
+    ModelAndView mv = new ModelAndView();
+    mv.addObject("currentUser", user);
+    mv.addObject("stock", stock);
+    mv.addObject("dateFormat", dateFormat);
+    mv.addObject("insertUpdate", "Insert");
+    mv.addObject("fullName", user.getFullname());
+    mv.addObject("dashView", adminDash);
+    mv.setViewName("insertUpdate");
 
-		model.addAttribute("currentUser", user);
-		model.addAttribute("dateFormat", dateFormat);
-		model.addAttribute("insertUpdate", "Update");
-		model.addAttribute("fullName", user.getFullname());
-		return "insertUpdate.html";
-	}
+    return mv;
+  }
 
-	/**
-	 * sectorAggregation accepts the parameter to create and run a mongoDB
-	 * Aggregation query getting the total number of outstanding shares for each
-	 * industry in the specified sector
-	 * 
-	 * @param sectorName Sector to aggregate
-	 * @param model      Model Object populated for view
-	 * @return
-	 */
-	@GetMapping("/sectorOutstandingShares")
-	private String sectorAggregation(@RequestParam(name = "sector", required = true) String sectorName, Model model) {
-		String stocksCollectionName = marketTemplate.getCollectionName(Stocks.class);
-		
-		/* Create the match Operation */
-		MatchOperation sectorMatchOp = Aggregation.match(new Criteria("Sector").is(sectorName));
-		/* Create the group Operation */
-		GroupOperation industryGroupOp = Aggregation.group("Industry").sum("Shares Outstanding").as("total");
+  @GetMapping("/updateStock")
+  public String updateStock(@RequestParam(name = "ticker", required = true) String ticker, Model model) {
 
-		/* Create the aggregation combining the mating and grouping operations */
-		Aggregation sectorIndustryAggregation = Aggregation.newAggregation(sectorMatchOp, industryGroupOp);
+    User user = marketHelper.getAuthenticatedUser();
+    Query query = new Query(Criteria.where("ticker").is(ticker));
+    Stocks stock = marketTemplate.findOne(query, Stocks.class);
+    model.addAttribute("stock", stock);
 
-		/* Run the Aggregation */
-		AggregationResults<SectorIndustryOutstandingShares> aggResults = marketTemplate
-				.aggregate(sectorIndustryAggregation, stocksCollectionName, SectorIndustryOutstandingShares.class);
-		List<SectorIndustryOutstandingShares> sharesResults = aggResults.getMappedResults();
-		
-		User user = marketHelper.getAuthenticatedUser();
+    model.addAttribute("currentUser", user);
+    model.addAttribute("dateFormat", dateFormat);
+    model.addAttribute("insertUpdate", "Update");
+    model.addAttribute("fullName", user.getFullname());
+    model.addAttribute("dashView", adminDash);
+    
+    return "insertUpdate.html";
+  }
 
-		model.addAttribute("currentUser", user);
-		model.addAttribute("sectorName", sectorName);
-		model.addAttribute("fullName", user.getFullname());
-		model.addAttribute("outstandingShares", sharesResults);
+  /**
+   * sectorAggregation accepts the parameter to create and run a mongoDB
+   * Aggregation query getting the total number of outstanding shares for each
+   * industry in the specified sector
+   * 
+   * @param sectorName Sector to aggregate
+   * @param model Model Object populated for view
+   * @return
+   */
+  @GetMapping("/sectorOutstandingShares")
+  private String sectorAggregation(@RequestParam(name = "sector", required = true) String sectorName, Model model) {
+    String stocksCollectionName = marketTemplate.getCollectionName(Stocks.class);
 
-		return "outstandingShares.html";
-	}
-	
-	/**
-	 * method to facilitate the uploading of data contained in csv files to the stocks collection
-	 * 
-	 * @param uploadfile
-	 * @param fromWeb
-	 * @param httpServletResponse
-	 * @return
-	 */
-	@RequestMapping(value = "/UploadCsvFile", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN_VALUE)
-	public ModelAndView updloadStocksCsv(@RequestParam("file") MultipartFile uploadfile,
-			@RequestParam(name = "fromWeb", required = false) Optional<Boolean> fromWeb,
-			HttpServletResponse httpServletResponse) {
-		
-		List<Stocks> stocksToSave = null;
-		try {
-			stocksToSave = marketHelper.convertCSVToStocks(uploadfile.getInputStream());
-			
-			marketRepository.insert(stocksToSave);
-		} catch (IOException | ParseException e) {
-			marketHelper.redirectToError(httpServletResponse, e.getMessage());
-		}
-		
-		return this.dashboard();
-	}
+    /* Create the match Operation */
+    MatchOperation sectorMatchOp = Aggregation.match(new Criteria("Sector").is(sectorName));
+    /* Create the group Operation */
+    GroupOperation industryGroupOp = Aggregation.group("Industry").sum("Shares Outstanding").as("total");
+
+    /* Create the aggregation combining the mating and grouping operations */
+    Aggregation sectorIndustryAggregation = Aggregation.newAggregation(sectorMatchOp, industryGroupOp);
+
+    /* Run the Aggregation */
+    AggregationResults<SectorIndustryOutstandingShares> aggResults = marketTemplate.aggregate(sectorIndustryAggregation, stocksCollectionName, SectorIndustryOutstandingShares.class);
+    List<SectorIndustryOutstandingShares> sharesResults = aggResults.getMappedResults();
+
+    User user = marketHelper.getAuthenticatedUser();
+
+    model.addAttribute("currentUser", user);
+    model.addAttribute("sectorName", sectorName);
+    model.addAttribute("fullName", user.getFullname());
+    model.addAttribute("outstandingShares", sharesResults);
+    if (marketHelper.isUserAdmin(user)) {
+      model.addAttribute("dashView", adminDash);
+    }
+    else {
+      model.addAttribute("dashView", userDash);
+    }
+
+    return "outstandingShares.html";
+  }
+
+  /**
+   * method to facilitate the uploading of data contained in csv files to the stocks collection
+   * 
+   * @param uploadfile
+   * @param fromWeb
+   * @param httpServletResponse
+   * @return
+   */
+  @RequestMapping(value = "/UploadCsvFile", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN_VALUE)
+  public ModelAndView updloadStocksCsv(@RequestParam("file") MultipartFile uploadfile, @RequestParam(name = "fromWeb", required = false) Optional<Boolean> fromWeb, HttpServletResponse httpServletResponse) {
+
+    List<Stocks> stocksToSave = null;
+    try {
+      stocksToSave = marketHelper.convertCSVToStocks(uploadfile.getInputStream());
+
+      marketRepository.insert(stocksToSave);
+    }
+    catch(IOException | ParseException e) {
+      marketHelper.redirectToError(httpServletResponse, e.getMessage());
+    }
+
+    return this.dashboard();
+  }
 
 }
